686cab5d5d9d1766dd94a82b38a6303345ebab8d
kati <fanomezantsoakatiuscia048@gmail.com>
2020-10-30 11:45:23 +0300

    generate the quotes

--- Pages/RandomQuotes.js
+++ Pages/RandomQuotes.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
-import Quotes from "./Quotes"
 
-const API = "https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10"
+const API = "https://quote-garden.herokuapp.com/api/v2/quotes/random"
 
 function RandomQuotes() {
+    
     const[quotes, setQuotes] = useState([]);
 
- 
     const getQuotes = async () => { 
             const res = await fetch(API);
-            // console.log(res);
             const listOfQuotes = await res.json();
-            setQuotes(listOfQuotes.quotes);// to access the value inside of
-        
+            console.log(listOfQuotes.quote);
+            setQuotes(listOfQuotes.quote);  // to access the value inside of an object
+            
     }
 
     useEffect(() => {
         getQuotes();
     }, []);
-    console.log(quotes);
+
+    console.log(quotes.quoteText);
+
     return(
-    <div>{quotes.map((quote) => (<Quotes key={quote.id} quote={quote} />))}</div>
+        <div>
+            <button className="quote-button" onClick={getQuotes}>Random</button>
+            <p className="quote">{quotes.quoteText}</p>
+        </div>
     )
 }
 
